541b4d2af2701c5c762cbb2f7571aafed7b37b2f
Philip Ayoub <philip@method.security>
2025-02-18 11:57:34 -0500

    smtp enumerate tool

---
+++ cmd/smtp.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"github.com/Method-Security/networkscan/internal/smtp"
+	"github.com/spf13/cobra"
+)
+
+func (a *NetworkScan) InitSMTPCommand() {
+	smtpCmd := &cobra.Command{
+		Use:   "smtp",
+		Short: "SMTP into a target host",
+		Long:  "SMTP into a target host",
+	}
+
+	smtpEnumerateCmd := &cobra.Command{
+		Use:   "enumerate",
+		Short: "Enumerate data about SMTP on a target host",
+		Long:  `Enumerate data about SMTP on a target host`,
+		Run: func(cmd *cobra.Command, args []string) {
+
+			targets, err := cmd.Flags().GetStringSlice("targets")
+			if err != nil {
+				a.OutputSignal.AddError(err)
+				return
+			}
+
+			timeout, err := cmd.Flags().GetInt("timeout")
+			if err != nil {
+				a.OutputSignal.AddError(err)
+				return
+			}
+
+			report, err := smtp.RunSMTPEnumerate(cmd.Context(), targets, timeout)
+			if err != nil {
+				a.OutputSignal.AddError(err)
+				return
+			}
+			a.OutputSignal.Content = report
+		},
+	}
+
+	smtpEnumerateCmd.Flags().StringSlice("targets", []string{}, "Target IP Socket or FQDN Socket to enumerate")
+	smtpEnumerateCmd.Flags().Int("timeout", 30, "Total time allowed for enumeration of each target in seconds")
+	_ = smtpEnumerateCmd.MarkFlagRequired("targets")
+
+	smtpCmd.AddCommand(smtpEnumerateCmd)
+	a.RootCmd.AddCommand(smtpCmd)
+}
