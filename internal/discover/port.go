e4d99186612275bef4812278920309cfad89a164
apurvagoenka-method <agoenka@method.security>
2025-05-19 16:58:57 -0400

    UDP Port Scan option (#62)
    * review improvements
    
    * gRPC custom plugin
    
    * gRPC enumerate
    
    * first order UDP scan options
    
    * improvements

--- internal/discover/port.go
+++ internal/discover/port.go
@@ -2,6 +2,7 @@ package discover
 
 import (
 	"context"
+	"strings"
 
 	discoverFern "github.com/Method-Security/networkscan/generated/go/discover"
 	"github.com/projectdiscovery/goflags"
@@ -11,11 +12,11 @@ import (
 
 // RunPortScan takes a target host and a list of ports to scan and returns a report of all hosts that were scanned and
 // their open ports.
-func RunPortScan(ctx context.Context, target string, ports string, topport string, threads int, scantype string) (*discoverFern.DiscoverPortReport, error) {
-	resources := discoverFern.DiscoverPortReport{}
+func RunPortScan(ctx context.Context, config discoverFern.DiscoverPortConfig) (*discoverFern.DiscoverPortReport, error) {
+	resources := discoverFern.DiscoverPortReport{Config: &config}
 	errors := []string{}
 
-	portscanResult, err := getPortScan(ctx, target, ports, topport, threads, scantype)
+	portscanResult, err := getPortScan(ctx, config)
 	if err != nil {
 		errors = append(errors, err.Error())
 	}
@@ -25,7 +26,7 @@ func RunPortScan(ctx context.Context, target string, ports string, topport strin
 	return &resources, nil
 }
 
-func getPortScan(ctx context.Context, target string, ports string, topports string, threads int, scantype string) ([]*discoverFern.DiscoverSocketDetails, error) {
+func getPortScan(ctx context.Context, config discoverFern.DiscoverPortConfig) ([]*discoverFern.DiscoverSocketDetails, error) {
 	output := result.HostResult{}
 	hosts := []*discoverFern.DiscoverSocketDetails{}
 	// These settings mimic naabu's default settings
@@ -35,9 +36,9 @@ func getPortScan(ctx context.Context, target string, ports string, topports stri
 		NoColor:           true,
 		Rate:              runner.DefaultRateConnectScan,
 		Retries:           runner.DefaultRetriesConnectScan,
-		Threads:           threads,
+		Threads:           config.Threads,
 		Timeout:           runner.DefaultPortTimeoutConnectScan,
-		Host:              goflags.StringSlice{target},
+		Host:              goflags.StringSlice{config.Target},
 		SkipHostDiscovery: true,
 		WarmUpTime:        2,
 		InputReadTimeout:  180000000000, // This is their default
@@ -47,20 +48,33 @@ func getPortScan(ctx context.Context, target string, ports string, topports stri
 		},
 	}
 
-	switch scantype {
+	switch config.ScanType {
 	case "syn":
 		portscanOpts.ScanType = runner.SynScan
 	case "connect":
 		portscanOpts.ScanType = runner.ConnectScan
-	default:
-		portscanOpts.ScanType = ""
 	}
 
-	if ports != "" {
-		portscanOpts.Ports = ports
+	// Combine tcpPorts and udpPorts into a single string for portscanOpts.Ports
+	var portList []string
+	if config.TcpPorts != nil {
+		portList = append(portList, *config.TcpPorts)
 	}
-	if topports != "" {
-		portscanOpts.TopPorts = topports
+	if config.UdpPorts != nil {
+		udpParts := strings.Split(*config.UdpPorts, ",")
+		for _, udp := range udpParts {
+			udp = strings.TrimSpace(udp)
+			if udp != "" {
+				portList = append(portList, "u:"+udp)
+			}
+		}
+	}
+	if len(portList) > 0 {
+		portscanOpts.Ports = strings.Join(portList, ",")
+	}
+
+	if config.TopPorts != nil {
+		portscanOpts.TopPorts = *config.TopPorts
 	}
 
 	portscan, err := runner.NewRunner(portscanOpts)
