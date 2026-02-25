402803627b5ee7c2f7504a9fa191372a66781455
Philip Ayoub <philip@method.security>
2026-02-25 09:48:16 -0500

    fix string

--- internal/discover/port/port.go
+++ internal/discover/port/port.go
@@ -96,7 +96,7 @@ func RunPortScan(ctx context.Context, config discoverfern.DiscoverPortConfig) (*
 				log.Warn("Number of open ports exceeds validation threshold, triggering validation",
 					svc1log.SafeParam("openPorts", openPortCount),
 					svc1log.SafeParam("threshold", *config.MaxOpenPortsValidationThreshold))
-				errors = append(errors, fmt.Sprintf("validation triggered due to count of open ports exceed (%d > %d)", openPortCount, *config.MaxOpenPortsValidationThreshold)) // Note: DD Metrics is generated off of this error line. Please update with caution
+				errors = append(errors, fmt.Sprintf("validation triggered due to count of open ports exceeding threshold (%d > %d)", openPortCount, *config.MaxOpenPortsValidationThreshold)) // Note: DD Metrics is generated off of this error line. Please update with caution
 				shouldValidate = true
 			}
 		}
